6ec4d85a4b5e312ba97289125be183a48fc51aba
benjaminlemon <benjamin.lemon1@gmail.com>
2023-10-27 09:43:25 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = '33a8e75d-27cf-44f8-a6e4-5453670d1089';
+export const MODAL_CLOSE_SEC = 2.5;
