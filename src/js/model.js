30f5e61e352333e3af2aca7cd96f99643e52b1c8
benjaminlemon <benjamin.lemon1@gmail.com>
2023-10-27 13:18:26 -0600

    model.js edit4

--- src/js/model.js
+++ src/js/model.js
@@ -19,7 +19,7 @@ const createRecipeObject = function (data) {
     title: recipe.title,
     publisher: recipe.publisher,
     sourceUrl: recipe.source_url,
-    image: recipe.image_url
+    image: recipe.image_url,
     servings: recipe.servings,
     cookingTime: recipe.cooking_time,
     ingredients: recipe.ingredients,
